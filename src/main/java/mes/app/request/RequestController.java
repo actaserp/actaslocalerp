b7a5678c48297ba099d041813769f7e0f41349cb
LeeJaeWook <ljw960214@naver.com>
2025-11-28 17:10:39 +0900

    20251128_이재욱_유지보수 화면작업

--- src/main/java/mes/app/request/RequestController.java
+++ src/main/java/mes/app/request/RequestController.java
@@ -3,12 +3,24 @@ package mes.app.request;
 import lombok.extern.slf4j.Slf4j;
 import mes.app.request.service.RequestService;
 import mes.domain.entity.TbAs011;
+import mes.domain.entity.User;
+import mes.domain.entity.commute.TB_PB201;
+import mes.domain.model.AjaxResult;
 import mes.domain.repository.TbAs010Repository;
 import mes.domain.repository.TbAs011Repository;
 import mes.domain.repository.TbAs020Repository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 @Slf4j
 @RestController
@@ -26,5 +38,33 @@ public class RequestController {
 
     @Autowired
     private TbAs020Repository tbAs020Repository;
+
+    // 사용자 정보 조회(부서 이름 출근여부)
+    @GetMapping("/search")
+    public AjaxResult searchDatas(
+            HttpServletRequest request,
+            @RequestParam(value="searchfrdate") String searchfrdate,
+            @RequestParam(value="searchtodate") String searchtodate,
+            @RequestParam(value="searchCompCd", required=false) Integer searchCompCd,
+            @RequestParam(value="reqType", required=false) Integer reqType,
+            @RequestParam(value="spjangcd", required=false) String spjangcd,
+            Authentication auth) {
+        AjaxResult result = new AjaxResult();
+        User user = (User)auth.getPrincipal();
+        String username = user.getUsername();
+
+//        List<TB_PB201> searchData = tbAs010Repository.find(tbPb201Pk);
+        List<Map<String, Object>> searchDatas  = requestService.searchDatas(
+                searchfrdate
+                , searchtodate
+                , searchCompCd
+                , reqType
+                , spjangcd
+        );
+
+        result.data = searchDatas;
+
+        return result;
+    }
 
 }
--- src/main/java/mes/app/request/service/RequestService.java
+++ src/main/java/mes/app/request/service/RequestService.java
@@ -2,10 +2,43 @@ package mes.app.request.service;
 
 import mes.domain.services.SqlRunner;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
 
 @Service
 public class RequestService {
     @Autowired
     SqlRunner sqlRunner;
+
+    // 사용자 정보 조회
+    public List<Map<String, Object>> searchDatas(
+            String searchfrdate
+            , String searchtodate
+            , Integer searchCompCd
+            , Integer reqType
+            , String spjangcd
+    ) {
+
+        MapSqlParameterSource dicParam = new MapSqlParameterSource();
+        dicParam.addValue("searchfrdate", searchfrdate);
+        dicParam.addValue("searchtodate", searchtodate);
+        dicParam.addValue("searchCompCd", searchCompCd);
+        dicParam.addValue("reqType", reqType);
+        dicParam.addValue("spjangcd", spjangcd);
+
+        String sql = """
+                SELECT
+                * FROM tb_as010
+                  WHERE 1=1
+        		""";
+
+
+        List<Map<String, Object>> item = this.sqlRunner.getRows(sql, dicParam);
+
+        return item;
+    }
+
 }
