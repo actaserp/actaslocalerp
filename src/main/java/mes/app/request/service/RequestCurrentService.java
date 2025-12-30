c8d8e6edd400e660422b0718ece5ae23e22f9de2
LeeJaeWook <ljw960214@naver.com>
2025-12-30 18:30:55 +0900

    20251230_이재욱_유지보수 요청현황 조회 오류 수정

--- src/main/java/mes/app/request/service/RequestCurrentService.java
+++ src/main/java/mes/app/request/service/RequestCurrentService.java
@@ -107,7 +107,7 @@ public class RequestCurrentService {
         if (aspernm != null && !aspernm.isEmpty()) { // 본사담당 검색필터, 담당자 배정받지 않은 건도 표시
             dicParam.addValue("aspernm", aspernm.toString());
             dicParam.addValue("asperid", perId.toString());
-            sql += " AND (a.\"aspernm\" = :aspernm OR a.\"aspernm\" IS NULL OR a.\"asperid\" = :asperid ) ";
+            sql += " AND (a.\"aspernm\" = :aspernm OR a.\"aspernm\" IS NULL OR a.\"aspernm\" = '' OR a.\"asperid\" = :asperid) ";
         }
         if (searchtodate != null && !searchtodate.isEmpty()) {
             sql += " AND a.\"asdate\" <= :searchtodate ";
